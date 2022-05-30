b5097403581af21fe35633fac682e9ad27574306
Simão Bentes <sdgbentes@gmail.com>
2022-05-30 17:10:17 +0100

    db

--- app.js
+++ app.js
@@ -6,13 +6,7 @@ const indexRouter = require("./routes/index");
 const memoriaRouter = require("./routes/memoria");
 
 const port = process.env.PORT || 3000;
-
 const app = express();
-
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET */
 router.get("/", (req, res) => {
-  res.status(200).json({ api: "OverTV", msg: "Hello World!" });
+  res.status(200).json({ api: "OverTV", msg: "Olá!" });
 });
 
 module.exports = router;
