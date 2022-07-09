238a38d63ed9266950ecf06156e84c9aef7f330c
Simão Bentes <sdgbentes@gmail.com>
2022-07-09 18:26:06 +0100

    ndakjndsjk

--- routes/evento.js
+++ routes/evento.js
@@ -5,7 +5,7 @@ const authenticatedMiddleware = require("../middlewares/authenticatedMiddleware"
 
 router.get("/", eventoController.getAll);
 router.get("/saved", authenticatedMiddleware, eventoController.getSaved);
-router.get("/:idEvento", eventoController.getEvento);
+router.get("/:idEvento", authenticatedMiddleware, eventoController.getEvento);
 router.get("/:idEvento/action", authenticatedMiddleware, eventoController.action);
 router.get("/:idEvento/pub", authenticatedMiddleware, eventoController.getPub);
 
