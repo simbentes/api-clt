424dfc5bc8503437bedbd6ef64a11448aab2b3ad
Simão Bentes <sdgbentes@gmail.com>
2022-07-12 18:44:33 +0100

    sdfgsdfg

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET */
 router.get("/", (req, res) => {
-  res.status(200).json({ api: "OverTV", msg: "Olá!" });
+  res.status(200).json({ api: "OverTV", msg: "Olá!", version: "1.2.1" });
 });
 
 module.exports = router;
